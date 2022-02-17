3b72f324abe4ce3788db5a8e8dbfd69337962160
Cerubois <cerubois@gmail.com>
2022-02-17 14:54:59 -0500

    Update TollStation.ts

--- src/cards/base/TollStation.ts
+++ src/cards/base/TollStation.ts
@@ -27,12 +27,19 @@ export class TollStation extends Card implements IProjectCard {
       },
     });
   }
-  public play(player: Player) {
-    const amount = player.game.getPlayers()
-      .filter((aPlayer) => aPlayer !== player)
-      .map((opponent) => opponent.getTagCount(Tags.SPACE, 'raw'))
-      .reduce((a, c) => a + c, 0);
-    player.addProduction(Resources.MEGACREDITS, amount, {log: true});
-    return undefined;
-  }
+
+    public play(player: Player) {
+        //Find opponent with most space tags
+        //let allOpps = player.game.getPlayers().filter((aPlayer) => aPlayer !== player)
+        //let mostOpp = allOpps.
+        //    .map((opponent) => opponent.getTagCount(Tags.SPACE, 'raw'));
+
+         const amount = player.game.getPlayers()
+              .filter((aPlayer) => aPlayer !== player)
+              .map((opponent) => opponent.getTagCount(Tags.SPACE, 'raw'))
+              .reduce((most, opp) => (most || 0) > opp ? most : opp, 0); // Testing this line to see if reduce works like this?
+              player.addProduction(Resources.MEGACREDITS, amount, {log: true});
+         return undefined;
+    }
+
 }
